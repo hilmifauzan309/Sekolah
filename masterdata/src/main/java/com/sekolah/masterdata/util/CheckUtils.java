db8e7b49108107725fba8f355be04c66c83a00a0
hilmifauzan309 <165632588+hilmifauzan309@users.noreply.github.com>
2024-05-04 09:09:44 +0700

    Delete masterdata directory

--- masterdata/src/main/java/com/sekolah/masterdata/util/CheckUtils.java
+++
@@ -1,7 +0,0 @@
-package com.sekolah.masterdata.util;
-
-public class CheckUtils {
-    public static boolean IsNullOrEmpty(String param){
-        return (param == null) || (param.equals(""));
-    }
-}
--- masterdata/src/main/java/com/sekolah/masterdata/util/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.sekolah.masterdata.util;
-
-public class Constant {
-    public static String STATUS = "status";
-    public static String TOKEN = "token";
-}
