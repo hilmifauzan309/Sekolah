5ecd7f7e89965ae5f605817151260dc9ed7ba812
hilmifauzan309 <165632588+hilmifauzan309@users.noreply.github.com>
2024-05-04 09:09:56 +0700

    Delete user directory

--- user/src/main/java/com/sekolah/user/util/CheckUtils.java
+++
@@ -1,7 +0,0 @@
-package com.sekolah.user.util;
-
-public class CheckUtils {
-    public static boolean IsNullOrEmpty(String param){
-        return (param == null) || (param.equals(""));
-    }
-}
--- user/src/main/java/com/sekolah/user/util/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.sekolah.user.util;
-
-public class Constant {
-    public static String STATUS = "status";
-    public static String TOKEN = "token";
-}
